da2c43c50ba0ef760749d0bd6ada01f1bea48fdd
NERONSW <nipunas98@gmail.com>
2025-12-15 15:01:08 +1100

    initial project structure

---
+++ src/app.ts
@@ -0,0 +1,56 @@
+import express, {
+  type Request,
+  type Response,
+  type NextFunction,
+} from "express";
+
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+// Custom error type to allow statusCode
+interface CustomError extends Error {
+  statusCode?: number;
+}
+
+// Express app creation
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Allowing CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+// Basic route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Backend running successfully!");
+});
+
+// 404 handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const error: CustomError = new Error("Endpoint not found");
+  error.statusCode = 404;
+  next(error);
+});
+
+// General error handler
+app.use(
+  (error: CustomError, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+
+    const statusCode = error.statusCode || 500;
+    const message = error.message || "An unknown error occurred";
+
+    res.status(statusCode).json({ error: message });
+  }
+);
+
+export default app;
